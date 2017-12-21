1d62aaf51d33198cf538dc9b6400493ebe860918
Kwon Sungbong <sb.kwon@anypointmedia.com>
2017-12-21 15:38:56 +0900

    test

--- src/main/java/com/example/demo/entity/TestEntity.java
+++ src/main/java/com/example/demo/entity/TestEntity.java
@@ -3,7 +3,10 @@ package com.example.demo.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.AUTO;
 
 /**
  * Created by whilemouse on 17. 11. 16.
@@ -13,7 +16,15 @@ import javax.persistence.Id;
 public class TestEntity {
 
     @Id
+    @GeneratedValue(strategy = AUTO)
     private Long id;
     private String test;
+
+    public TestEntity clone(boolean isCreate) {
+        TestEntity clone = new TestEntity();
+        clone.setId(isCreate ? null : this.getId());
+        clone.setTest(this.getTest());
+        return clone;
+    }
 
 }
--- src/test/java/com/example/demo/component/RawDataLoaderTests.java
+++ src/test/java/com/example/demo/component/RawDataLoaderTests.java
@@ -1,10 +1,15 @@
 package com.example.demo.component;
 
+import com.example.demo.entity.TestEntity;
+import com.example.demo.repository.TestRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
 import java.io.IOException;
 
 @RunWith(SpringRunner.class)
@@ -17,6 +22,30 @@ public class RawDataLoaderTests {
 
 		RawDataLoader.load(rawDataFileName);
 
+	}
+
+	@Autowired
+	TestRepository testRepository;
+
+	@Test
+	@Rollback(false)
+	public void test() {
+		TestEntity testEntity = new TestEntity();
+		testEntity.setTest("test");
+		testRepository.save(testEntity);
+	}
+
+	@Test
+	@Rollback(false)
+	public void test2() {
+		TestEntity testEntity = testRepository.findOne(1L);
+		test3(testEntity);
+	}
+
+	@Transactional
+	public void test3(TestEntity testEntity) {
+		TestEntity clone = testEntity.clone(true);
+		testRepository.save(clone);
 	}
 
 }
